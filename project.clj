38534b661cb61ff485ff6a3e0920d63b61bddadd
ntestoc3 <ntestoc3@outlook.com>
2020-02-28 23:49:59 +0800

    添加deep-diff输出html

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 ;; [lambdaisland/deep-diff "0.0-35"]
-                 ;; [mvxcvi/puget "1.2.0"] ;; pretty printer
+                 [lambdaisland/deep-diff "0.0-47"]
                  [jansi-clj "0.1.1"]
                  [org.apache.commons/commons-text "1.8"] ;; text diff
                  [org.bitbucket.cowwoc/diff-match-patch "1.2"] ;; google diff patch
---
+++ src/difft/ddiff_ext.clj
@@ -0,0 +1,38 @@
+(ns difft.ddiff-ext
+  (:require [puget.printer :as puget]
+            [lambdaisland.deep-diff.printer :as dprinter]
+            [lambdaisland.deep-diff :as ddiff]
+            [clojure.string :as str]))
+
+(def default-color-schema {::dprinter/deletion  [:red]
+                           ::dprinter/insertion [:green]
+                           ::dprinter/other     [:black]
+                           :nil       [:black]
+                           :number    [:black]
+                           :string    [:black]
+                           :character [:black]
+                           :symbol    nil
+                           :function-symbol [:black]
+                           :class-delimiter [:black]
+                           :class-name      [:black]
+                           :boolean    [:black]
+                           :keyword    [:black]
+                           :delimiter  [:black]
+                           :tag        [:black]})
+
+(defn cprint-str
+  ([diff] (cprint-str diff {:color-scheme default-color-schema}))
+  ([diff opts]
+   (->> opts
+        (dprinter/puget-printer)
+        (puget/cprint-str diff))))
+
+(defn print-html
+  ([diff] (print-html diff {:color-scheme default-color-schema
+                            :width 80}))
+  ([diff opts]
+   (let [r (->> (merge opts {:color-markup :html-inline})
+                (cprint-str diff))]
+     (-> (str/replace r #"(?im)\R" "<br/>")
+         (as-> $
+             (str "<div>" $ "</div>"))))))
